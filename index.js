0fdd7f00603447f7e2384a72fa4265bb2574e66d
Faizan7012 <frtoons7012@gmail.com>
2023-01-04 12:48:03 +0530

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+const JS = require('json-server');
+const server = JS.create();
+const router = JS.router('db.json');
+const middle = JS.defaults();
+
+
+server.use(middle)
+server.use(router)
+
+
+server.listen(8080)
