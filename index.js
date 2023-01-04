90ceff6ba926479b55436166889b277e0253f545
Faizan7012 <frtoons7012@gmail.com>
2023-01-04 15:32:40 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const JS = require('json-server');
 const router = JS.router('./db.json')
 const server = JS.create();
-const middle = JS.defaults();
+const middle = JS.defaults([
+    {
+        readOnly:false
+    }
+]);
 
 server.use(middle)
 server.use(router)
